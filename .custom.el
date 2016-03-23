302c0b9289d43f3faa3faf652f05703ac4841784
michael.y.ma@DCH.com <michael.y.ma@DCH.com>
2016-03-23 17:54:44 +0800

    Add customer config

---
+++ .custom.el
@@ -0,0 +1,15 @@
+;;; Automatically generated on Wed Mar 23 16:22:58 2016
+;;; Invoked by michael.y.ma@MICHAELYMA-PC using 24.5.1
+(setq-default occur-collect-regexp-history '("\\1"))
+(setq org-agenda-files (list "E:/Task/MyPlan/inbox.org"
+                             "E:/Task/MyPlan/project.org"
+                             "E:/Task/MyPlan/task.org"
+                             "E:/Task/MyPlan/trash.org"
+                             "E:/Task/MyPlan/finished.org"
+                             "E:/Task/MyPlan/note.org"
+                           ))
+(setq org-refile-targets (quote (("task.org" :level . 1)
+                                 ("project.org" :level . 1)
+                                 ("trash.org" :level . 1)
+                                 ("finished.org" :level . 1)
+                                 ("note.org" :level . 1))))
